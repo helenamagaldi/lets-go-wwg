9b74cb65d113e218faf80529ccc9b488ca549147
Helena Magaldi <helenamagaldi@gmail.com>
2021-07-22 10:09:25 -0300

    feat new extra exercises

---
+++ Extra Exercises/Composed/exercise1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	yellow := [5]string{"Fernando", "João", "Lúcia", "Mariana", "Ana"}
+	red := [4]string{"Helena", "Jonas", "José", "Juliana"}
+
+	fmt.Println(yellow, red)
+}
